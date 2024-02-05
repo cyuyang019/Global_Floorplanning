15a4a12c43b8be0919fa3b427ae206f554dae853
cyuyangOnMacbook <cyuyang019@gmail.com>
2024-02-06 01:19:22 +0800

    able to record the results of different parameters

---
+++ utils/record_parameter.py
@@ -0,0 +1,33 @@
+import sys
+import csv
+import os
+
+case_name = sys.argv[1]
+log_path = "./log/" + case_name + ".log"
+csv_path = "./parameter_record/" + case_name + ".csv"
+
+if not os.path.exists(csv_path):
+    with open(csv_path, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["punishment", "aspect_ratio", "overlap_ratio", "HPWL"])
+
+fread = open(log_path, 'r')
+f = fread.read().split("\n")
+
+punishment_line = f[2]
+punishment_list = punishment_line.split(" ")
+punishment = float(punishment_list[-1])
+
+aspect_ratio_line = f[3]
+aspect_ratio_list = aspect_ratio_line.split(" ")
+aspect_ratio = float(aspect_ratio_list[-1])
+
+for line in f:
+    if "Overlap Ratio:" in line:
+        overlap_ratio = line.split()[-1]
+    if "Estimated HPWL:" in line:
+        HPWL = float(line.split()[-1])
+
+with open(csv_path, 'a', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow([punishment, aspect_ratio, overlap_ratio, HPWL])
